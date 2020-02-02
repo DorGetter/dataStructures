f422d1333ccad94da313896462cb1d6aed541f2e
DorGetter <57187365+DorGetter@users.noreply.github.com>
2020-02-02 20:38:29 +0200

    toString

--- src/F_AirplaineProblem/BestPath.java
+++ src/F_AirplaineProblem/BestPath.java
@@ -8,9 +8,10 @@ import F_AirplaineProblem.Node;;
 public class BestPath {
 
 	Node[][]mat; 
+	Node[][]Clone;
 	int teta;
-	ArrayList<String> paths = new ArrayList<String>();
-
+	int numOfPaths =0;
+	ArrayList<String> paths = new  ArrayList<String>(numOfPaths);
 	public BestPath(Node[][]mat, int teta) {
 		this.mat=mat; 
 		this.teta=teta; 
@@ -64,93 +65,85 @@ public class BestPath {
 			matPrint(matt);
 		}
 
-
+		this.numOfPaths= matt[0][matt[0].length-1].getCounterP1();
 		return matt[0][matt[0].length-1].getCounterP1();
 
 	}
 
 	public int getNumOfOptimalPaths() {
 
-		Node [][] matt = this.mat.clone();
-		matPrint(matt);
+		Clone = this.mat.clone();
+		matPrint(Clone);
 
-		matt[matt.length-1][0].setMinP1(0);
-		matt[matt.length-1][0].setCounterP1(0);
+		Clone[Clone.length-1][0].setMinP1(0);
+		Clone[Clone.length-1][0].setCounterP1(0);
 
+		//init row
+		for (int i = 1; i < Clone[0].length; i++) {
+			Clone[Clone.length-1][i].setMinP1(Clone[Clone.length-1][i-1].getX()+Clone[Clone.length-1][i-1].getMinP1());    
+			Clone[Clone.length-1][i].CounterturnP1=0;
+			Clone[Clone.length-1][i].lastTurnP1=0;
+			Clone[Clone.length-1][i].setCounterP1(1);  
+		}	matPrint(Clone);
+		//init col
+		for (int i =Clone.length-2; i >= 0 ; i--) {
+			Clone[i][0].setMinP1(Clone[i+1][0].getY()+Clone[i+1][0].getMinP1());
+			Clone[i][0].CounterturnP1=0;
+			Clone[i][0].lastTurnP1=1;
+			Clone[i][0].setCounterP1(1);
 
-		for (int i = 1; i < matt[0].length; i++) {
-			matt[matt.length-1][i].setMinP1(matt[matt.length-1][i-1].getX()+matt[matt.length-1][i-1].getMinP1());    
-			matt[matt.length-1][i].CounterturnP1=0;
-			matt[matt.length-1][i].lastTurnP1=0;
-			matt[matt.length-1][i].setCounterP1(1);  
-		}	matPrint(matt);
-
-		for (int i =matt.length-2; i >= 0 ; i--) {
-			matt[i][0].setMinP1(matt[i+1][0].getY()+matt[i+1][0].getMinP1());
-			matt[i][0].CounterturnP1=0;
-			matt[i][0].lastTurnP1=1;
-			matt[i][0].setCounterP1(1);
-
-		}	matPrint(matt);
-
-
+		}	matPrint(Clone);
 
 
-		for (int i = matt.length-2; i >= 0; i--) 
+
+		//go over the matrix:
+		for (int i = Clone.length-2; i >= 0; i--) 
 		{
-			for (int j = 1; j < matt[0].length; j++) 
+			for (int j = 1; j < Clone[0].length; j++) 
 			{			
 
-
-
-
-
 				//if its best to come from left : (by value)
-				if(matt[i][j-1].getMinP1()+matt[i][j-1].getX() < matt[i+1][j].getMinP1()+matt[i+1][j].getY())
+				if(Clone[i][j-1].getMinP1()+Clone[i][j-1].getX() < Clone[i+1][j].getMinP1()+Clone[i+1][j].getY())
 				{
-					matt[i][j].setMinP1(matt[i][j-1].getMinP1()+matt[i][j-1].getX()); //set new value to me.
-					matt[i][j].setCounterP1((matt[i][j-1].getCounterP1()));
+					Clone[i][j].setMinP1(Clone[i][j-1].getMinP1()+Clone[i][j-1].getX()); //set new value to me.
+					Clone[i][j].setCounterP1(Clone[i][j-1].getCounterP1());
 
 					//update last turn + counter// 
-					if(matt[i][j-1].getLastTurnP1()==1) {
+					if(Clone[i][j-1].getLastTurnP1()==1) {
 						//need to take a turn to advance to me : 
 						//update counter of the turns && set lest turn to 0
-						matt[i][j].CounterturnP1=matt[i][j-1].getCounterturnP1()+1;
-						matt[i][j].setLastTurnP1(0);
+						Clone[i][j].CounterturnP1=Clone[i][j-1].getCounterturnP1()+1;
+						Clone[i][j].setLastTurnP1(0);
 
 					}
 					else 
 					{
 						//Don't need to take a turn to advance to me : 
 						//update counter of the turns && set lest turn to 0
-						matt[i][j].CounterturnP1=matt[i][j-1].getCounterturnP1();
-						matt[i][j].setLastTurnP1(0);
+						Clone[i][j].CounterturnP1=Clone[i][j-1].getCounterturnP1();
+						Clone[i][j].setLastTurnP1(0);
 					}
 
 				}
 
-
-
-
-
 				//if its best to come from bottom : (by value)
-				else if(matt[i][j-1].getMinP1()+matt[i][j-1].getX() > matt[i+1][j].getMinP1()+matt[i+1][j].getY())
+				else if(Clone[i][j-1].getMinP1()+Clone[i][j-1].getX() > Clone[i+1][j].getMinP1()+Clone[i+1][j].getY())
 				{
-					matt[i][j].setMinP1(matt[i+1][j].getMinP1()+matt[i+1][j].getY());
-					matt[i][j].setCounterP1((matt[i+1][j].getCounterP1()));
+					Clone[i][j].setMinP1(Clone[i+1][j].getMinP1()+Clone[i+1][j].getY());
+					Clone[i][j].setCounterP1(Clone[i+1][j].getCounterP1());
 
 					//need to take a turn to advance to me : 
 					//update counter of the turns && set lest turn to 1
-					if(matt[i+1][j].getLastTurnP1()==0) {
-						matt[i][j].CounterturnP1=matt[i+1][j].getCounterturnP1()+1;
-						matt[i][j].setLastTurnP1(1);
+					if(Clone[i+1][j].getLastTurnP1()==0) {
+						Clone[i][j].CounterturnP1=Clone[i+1][j].getCounterturnP1()+1;
+						Clone[i][j].setLastTurnP1(1);
 					}
 					else 
 					{
 						//Don't need to take a turn to advance to me : 
 						//update counter of the turns && set lest turn to 1
-						matt[i][j].CounterturnP1=matt[i+1][j].getCounterturnP1();
-						matt[i][j].setLastTurnP1(1);
+						Clone[i][j].CounterturnP1=Clone[i+1][j].getCounterturnP1();
+						Clone[i][j].setLastTurnP1(1);
 					}
 
 				}
@@ -170,15 +163,16 @@ public class BestPath {
 					//		^		    ^
 					//		|	    	|
 					//		|  -------...
-					if(matt[i][j-1].getLastTurnP1()==1 && matt[i+1][j].getLastTurnP1()==0) {
+					if(Clone[i][j-1].getLastTurnP1()==1 && Clone[i+1][j].getLastTurnP1()==0) {
 
-						matt[i][j].setMinP1(matt[i][j-1].getMinP1()+matt[i][j-1].getX()); //set value
+						Clone[i][j].setMinP1(Clone[i][j-1].getMinP1()+Clone[i][j-1].getX()); //set value
 
-						matt[i][j].CounterturnP1=matt[i][j-1].getCounterturnP1()+1; //update turn Counter
+						Clone[i][j].CounterturnP1=Clone[i][j-1].getCounterturnP1()+1; //update turn Counter
 
 						//took from x -->
-						matt[i][j].setLastTurnP1(0);
-						matt[i][j].setCounterP1((matt[i+1][j].getCounterP1()+matt[i][j-1].getCounterP1()));
+						Clone[i][j].setLastTurnP1(0);
+
+						Clone[i][j].setCounterP1((Clone[i+1][j].getCounterP1()));
 
 					}
 
@@ -192,29 +186,29 @@ public class BestPath {
 					//						.
 					//						.
 
-					else if(matt[i][j-1].getLastTurnP1()==0 && matt[i+1][j].getLastTurnP1()==1) {
+					else if(Clone[i][j-1].getLastTurnP1()==0 && Clone[i+1][j].getLastTurnP1()==1) {
 
 
 						//CHECK IF -- less cheap (by steps) to come from the left :
-						if(matt[i][j-1].getCounterturnP1() < matt[i+1][j].getCounterturnP1())
+						if(Clone[i][j-1].getCounterturnP1() < Clone[i+1][j].getCounterturnP1())
 						{
-							matt[i][j].setMinP1(matt[i][j-1].getMinP1()+matt[i][j-1].getX());
-							matt[i][j].CounterturnP1=matt[i][j-1].getCounterturnP1();
+							Clone[i][j].setMinP1(Clone[i][j-1].getMinP1()+Clone[i][j-1].getX());
+							Clone[i][j].CounterturnP1=Clone[i][j-1].getCounterturnP1();
 
 							//took from x -->
-							matt[i][j].setLastTurnP1(0);
+							Clone[i][j].setLastTurnP1(0);
 
-							matt[i][j].setCounterP1(matt[i][j-1].getCounterP1());
+							Clone[i][j].setCounterP1(Clone[i][j-1].getCounterP1());
 
 						}
 						else
 						{
-							matt[i][j].setMinP1(matt[i+1][j].getMinP1()+matt[i+1][j].getY());
-							matt[i][j].CounterturnP1=matt[i+1][j].getCounterturnP1();
+							Clone[i][j].setMinP1(Clone[i+1][j].getMinP1()+Clone[i+1][j].getY());
+							Clone[i][j].CounterturnP1=Clone[i+1][j].getCounterturnP1();
 							// 				^
 							//took from y 	|
-							matt[i][j].setLastTurnP1(1);
-							matt[i][j].setCounterP1((matt[i+1][j].getCounterP1()));
+							Clone[i][j].setLastTurnP1(1);
+							Clone[i][j].setCounterP1((Clone[i+1][j].getCounterP1()));
 
 						}
 
@@ -234,36 +228,32 @@ public class BestPath {
 					else 
 					{
 						//IF FROM LEFT THERE WAS A TURN:
-						if(matt[i][j-1].getLastTurnP1()==1 && matt[i+1][j].getLastTurnP1()==1) {
+						if(Clone[i][j-1].getLastTurnP1()==1 && Clone[i+1][j].getLastTurnP1()==1) {
 
-							matt[i][j].setMinP1(matt[i+1][j].getMinP1()+matt[i+1][j].getY());
-							matt[i][j].CounterturnP1=matt[i+1][j].getCounterturnP1();
+							Clone[i][j].setMinP1(Clone[i+1][j].getMinP1()+Clone[i+1][j].getY());
+							Clone[i][j].CounterturnP1=Clone[i+1][j].getCounterturnP1();
 							//took from x -->
-							matt[i][j].setLastTurnP1(1);
-							matt[i][j].setCounterP1((matt[i+1][j].getCounterP1()));
+							Clone[i][j].setLastTurnP1(1);
+							Clone[i][j].setCounterP1((Clone[i+1][j].getCounterP1()));
 						}
 
+						else if(Clone[i][j-1].getLastTurnP1()==0 && Clone[i+1][j].getLastTurnP1()==0) {
 
-
-						else if(matt[i][j-1].getLastTurnP1()==0 && matt[i+1][j].getLastTurnP1()==0) {
-
-
-
-							matt[i][j].setMinP1(matt[i][j-1].getMinP1()+matt[i][j-1].getX());
-							matt[i][j].CounterturnP1=matt[i][j-1].getCounterturnP1();
+							Clone[i][j].setMinP1(Clone[i][j-1].getMinP1()+Clone[i][j-1].getX());
+							Clone[i][j].CounterturnP1=Clone[i][j-1].getCounterturnP1();
 							//took from x -->
-							matt[i][j].setLastTurnP1(0);
-							matt[i][j].setCounterP1((matt[i][j-1].getCounterP1()));
+							Clone[i][j].setLastTurnP1(0);
+							Clone[i][j].setCounterP1((Clone[i][j-1].getCounterP1()));
 						}
 					}
 
 				}
 			}
-			matPrint(matt);
+			matPrint(Clone);
 		}
-		matPrint(matt);
+		matPrint(Clone);
 
-		return matt[0][matt[0].length-1].getCounterturnP1();
+		return Clone[0][Clone[0].length-1].getCounterP1();
 	}
 
 	public double getCheapestPrice() {
@@ -308,7 +298,6 @@ public class BestPath {
 		}
 		return matt[0][matt[0].length-1].getMinP1();
 	}
-
 
 	public int printNumOfTurns() {
 
@@ -432,58 +421,115 @@ public class BestPath {
 	}
 
 	public ArrayList<String> getAllCheapestPaths(){
-		int t = getNumOfOptimalPaths();
-		allPathsRec();
+
+		getNumOfCheapestPaths();
+		paths = new ArrayList<String>(); 
+		buildPathsCheap(new String(), 0, mat[0].length-1,paths);
+		return paths; 
 	}
 
+	public void buildPathsCheap(String path, int i , int j ,ArrayList<String> paths)  {
 
-	public void buildPaths(String path, int i , int j ,ArrayList<String> paths)  {
+		if (i<mat.length-1 && j>0) {
+			double a  = mat[i+1][j].getMinP1();
+			double a2 = mat[i+1][j].y; 
+			double b  = mat[i][j-1].MinP1;
+			double b2 = mat[i][j-1].x ;
+			double c = mat[i][j].getMinP1();
 
-		if (i>0 && j>0) {
-			double a = mat[i-1][j].MinP1+mat[i-1][j].y;  
-			double b = mat[i][j-1].MinP1+mat[i][j-1].x ;
-			if (a < b) {   
-				buildPaths("1"+path, i-1, j, paths);
+			if (a+a2 == b+b2){   //a==b 
+				buildPathsCheap("1"+path, i+1, j, paths); 
+				buildPathsCheap("0" + new String(path), i, j-1, paths) ;
 			}
-			else if(a > b)   {
-				buildPaths("0"+path, i, j-1, paths);
+			else if (c-b2 == b) {   
+				buildPathsCheap("0"+path, i, j-1, paths);
 			}
-			else {//a==b 
-				buildPaths("1"+path, i-1, j, paths); 
-				buildPaths("0" + new String(path), i, j-1, paths) 
+			else if(c-a2 == a)   {
+				buildPathsCheap("1"+path, i+1, j, paths);
 			}
+
 		}
 
-		else if (i==0 && j==0)  { 
+		else if (i==mat.length-1 && j==0)  { 
 			paths.add(path);  
 		}
-		else if (i==0) {
+		else if (i==mat.length-1) {
 			String t = new String() ;
 
-			for  (int k=0; k< j-1 ;j++){ 
+			for  (int k=j-1; k >=0 ;k--){ 
 				t = t +  "0"   ;
 				paths.add(t + path);  
 
 			}
-
+			return;
 		}
 		else if (j==0) {
 			String t = new String();
-			for  (int k=0; k <i-1; i++) {
+			for  (int k=i; k <= 0; k++) {
 				t = t +  "1";
 				paths.add(t + path);
-			}
-		}
 
-		return buildPaths;
+			}
+			return;
+		}
+		return;
 	}
 
 
 	public ArrayList<String> getAllOptimalPaths() {
-		return null;
 
+		getNumOfOptimalPaths();
+		paths = new ArrayList<String>(); 
+		buildPathsOptimal(new String(), 0, mat[0].length-1,paths);
+		return paths; 
 	}
 
+	public void buildPathsOptimal(String path, int i , int j ,ArrayList<String> paths)  {
+
+		if (i<mat.length-1 && j>0) {
+			double a  = mat[i+1][j].getMinP1();
+			double a2 = mat[i+1][j].y; 
+			double b  = mat[i][j-1].MinP1;
+			double b2 = mat[i][j-1].x ;
+			double c = mat[i][j].getMinP1();
+			double num = mat[i][j].getCounterP1();
+			if (a+a2 == b+b2 && num > 1){   //a==b 
+				buildPathsOptimal("1"+path, i+1, j, paths); 
+				buildPathsOptimal("0" + new String(path), i, j-1, paths) ;
+			}
+			else if (c-b2 == b) {   
+				buildPathsOptimal("0"+path, i, j-1, paths);
+			}
+			else if(c-a2 == a)   {
+				buildPathsOptimal("1"+path, i+1, j, paths);
+			}
+
+		}
+
+		else if (i==mat.length-1 && j==0)  { 
+			paths.add(path);  
+		}
+		else if (i==mat.length-1) {
+			String t = new String() ;
+
+			for  (int k=j-1; k >=0 ;k--){ 
+				t = t +  "0"   ;
+				paths.add(t + path);  
+
+			}
+			return;
+		}
+		else if (j==0) {
+			String t = new String();
+			for  (int k=i; k <= 0; k++) {
+				t = t +  "1";
+				paths.add(t + path);
+
+			}
+			return;
+		}
+		return;
+	}
 
 	public int getNumOfCheapestPaths2() {
 		return 0;}
@@ -516,7 +562,7 @@ public class BestPath {
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m.length; j++) {
 
-				System.out.print("P1: "+m[i][j].getMinP1()+", C1: "+m[i][j].getCounterturnP1()+" LT: "+m[i][j].getLastTurnP1()+"Tnum:"+m[i][j].getCounterturnP1()+"\t\t\t\t");
+				System.out.print("P1: "+m[i][j].getMinP1()+", C1: "+m[i][j].getCounterturnP1()+" LT: "+m[i][j].getLastTurnP1()+" Tnum:"+m[i][j].getCounterturnP1()+"count"+m[i][j].getCounterP1()+"\t\t\t");
 			}
 			System.out.println("\n");
 		}
--- src/F_AirplaineProblem/mainRun.java
+++ src/F_AirplaineProblem/mainRun.java
@@ -1,16 +1,18 @@
 package F_AirplaineProblem;
 
+import java.util.ArrayList;
+
 public class mainRun {
 	public static void main(String[] args) {
 
-		
+
 		Node [][] mat = new Node [4][4];
-		
+
 		Node one = new Node(2,Double.MAX_VALUE);		Node one2 = new Node(3,Double.MAX_VALUE);		Node one3 = new Node(5,Double.MAX_VALUE);		Node one4 = new Node(Double.MAX_VALUE,Double.MAX_VALUE);
 		Node one5 = new Node(4,10);						Node one6 = new Node(3,1);			Node one7 = new Node(1,4);		Node one8 = new Node(Double.MAX_VALUE,8);
 		Node one9 = new Node(2,5);						Node one10 = new Node(5,11);		Node one11 = new Node(3,1);		Node one12 = new Node(Double.MAX_VALUE,2);
 		Node one13 = new Node(1,3);						Node one14 = new Node(8,4);			Node one15 = new Node(3,8);		Node one16 = new Node(Double.MAX_VALUE,4);
-		
+
 		mat[0][0] = one;
 		mat[0][1] = one2;
 		mat[0][2] = one3;
@@ -27,20 +29,27 @@ public class mainRun {
 		mat[3][1] = one14;
 		mat[3][2] = one15;
 		mat[3][3] = one16;
-		
-	int teta =3;
-	BestPath BS = new BestPath(mat,teta);
-	
-	
-	//System.out.println(BS.getNumOfCheapestPaths());
-	
-System.out.println("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
-	
-	//System.out.println(BS.printNumOfTurns());
-//	System.out.println(BS.getNumOfOptimalPaths());
-	System.out.println(BS.getAllCheapestPaths());
+
+		int teta =3;
+		BestPath BS = new BestPath(mat,teta);
+
+
+		//System.out.println(BS.getNumOfCheapestPaths());
+
+		System.out.println("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+
+		//System.out.println(BS.printNumOfTurns());
+		//System.out.println(BS.getNumOfOptimalPaths());
+		//System.out.println(BS.printNumOfTurns());
+		ArrayList<String> paths = new ArrayList<String>();
+
+		//paths = BS.getAllCheapestPaths();
+		paths = BS.getAllOptimalPaths();
+		for (int i = 0; i < paths.size(); i++) {
+			System.out.println(i+" ["+paths.get(i)+"]");
+		}
 	}
-	
-	
-	
+
+
+
 }
